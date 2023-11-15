ec27114fe4374c3d7a65b3538f3c0577a5d19c64
nebrity50 <150687286+nebrity50@users.noreply.github.com>
2023-11-15 20:42:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const eurInput = document.querySelector('.euro')
+const rublInput = document.querySelector('.rubl')
+const hrnInput = document.querySelector('.hrivnya')
+const usdInput = document.querySelector('.dollar')
+const price12 = document.querySelector('.price12')
+const price6 = document.querySelector('.price6')
+const price3 = document.querySelector('.price3')
+const buyInput = document.querySelector('.buy')
+const main = document.querySelector('#main')
+const btn12 = document.querySelector('#btn12')
+
+eurInput.addEventListener('mouseenter', () => {
+    price12.textContent = '12€'
+    price6.textContent = '7€'
+    price3.textContent = '4.5€'
+})
+rublInput.addEventListener('mouseenter', () => {
+    price12.textContent = '1200₽'
+    price6.textContent = '700₽'
+    price3.textContent = '400₽'
+})
+hrnInput.addEventListener('mouseenter', () => {
+    price12.textContent = '480₴'
+    price6.textContent = '280₴'
+    price3.textContent = '160₴'
+})
+usdInput.addEventListener('mouseenter', () => {
+    price12.textContent = '13$'
+    price6.textContent = '8$'
+    price3.textContent = '5$'
+})
+btn12.addEventListener('mousedown', () => {
+    price12.textContent = '480₴'
+    price6.textContent = '280₴'
+    price3.textContent = '160₴'
+})
+
+
+buyInput.addEventListener('mousedown', () => {
+    
+})
